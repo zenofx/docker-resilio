d821fbb27ce103e3d545b7f2965d4c2518bde045
Thorsten Schubert <tschubert@bafh.org>
2018-03-12 19:01:53 +0100

    minor fixes

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,21 @@ LABEL maintainer="Thorsten Schubert <tschubert@bafh.org>"
 ARG DEBIAN_FRONTEND="noninteractive"
 ARG SYNC_ARCH="x64"
 ARG SYNC_VER="stable"
+ARG UNAME="rslsync"
+ARG TZ="Europe/Berlin"
+ARG UID="1002"
+ARG GID="1002"
+ARG CHANGE_SYNC_DIR_OWNERSHIP="false"
 
-ENV UNAME=rslsync \
-	UID=1002 \
-	GID=1002 \
+ENV UNAME=${UNAME} \
 	SYNC_ARCH=${SYNC_ARCH} \
 	SYNC_VER=${SYNC_VER} \
-	TZ=Europe/Berlin
+	TZ=${TZ} \
+	UID=${UID} \
+	GID=${GID} \
+	CHANGE_SYNC_DIR_OWNERSHIP=${CHANGE_SYNC_DIR_OWNERSHIP}
+
+ENV TERM="xterm" LANG="C.UTF-8" LC_ALL="C.UTF-8"
 
 RUN \
 	apt-get update \
@@ -20,16 +28,20 @@ RUN \
 		openssl \
 		ca-certificates \
 		ssl-cert \
-		iproute2 \
-	&& rm -rf /var/lib/apt/lists/* /usr/share/man/* /tmp/*
+		busybox \
+	&& apt-get clean \
+	&& rm -rf /var/lib/apt/lists/* /usr/share/man/* /tmp/* /var/tmp/*
 
 COPY ./entrypoint.sh /entrypoint.sh
 COPY ./su-exec-0.2/su-exec /usr/local/bin/
 COPY ./root/sync.conf /config/sync.conf
 
 RUN \
-	mkdir -p /config /sync /app \
-	&& useradd -M -d /app -r -U -u ${UID} -s /bin/false ${UNAME} \
+	echo ${TZ} > /etc/timezone \
+	&& dpkg-reconfigure -f noninteractive tzdata 2>&1 \
+	&& mkdir -p /config /sync /app \
+	&& groupadd -r -g ${GID} ${UNAME} \
+	&& useradd -M -d /app -r -u ${UID} -g ${GID} -s /bin/false ${UNAME} \
 	&& chown -vR ${UID}:${GID} /config /sync /app \
 	&& chmod a+x /entrypoint.sh /usr/local/bin/su-exec
 
@@ -39,7 +51,6 @@ USER root
 
 ENTRYPOINT [ "/entrypoint.sh" ]
 CMD [ "/app/rslsync", "--nodaemon", "--config", "/config/sync.conf" ]
-
 
 # ports and volumes
 EXPOSE 3838/udp 50890/tcp 50890/udp 8890/tcp
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
+
 set -e
 
-function rsinst() {
-	echo "Downloading resilio-snc ${SYNC_VER} for ${SYNC_ARCH}."
+log()
+{
+    echo \[$(date +'%Y%m%d %H:%M:%S.%3N')\] $HOSTNAME $*
+}
+
+rsinst()
+{
+	log "downloading resilio-sync ${SYNC_VER} for ${SYNC_ARCH}."
 	su-exec $UID:$GID \
 	curl -s -o /app/version.txt -L "https://download-cdn.getsync.com/stable/linux-x64/version.txt" \
-	&& curl -s -o /app/sync.tar.gz -L "https://download-cdn.getsync.com/${SYNC_VER}/linux-${SYNC_ARCH}/resilio-sync_${SYNC_ARCH}.tar.gz" \
+		&& curl -s -o /app/sync.tar.gz -L "https://download-cdn.getsync.com/${SYNC_VER}/linux-${SYNC_ARCH}/resilio-sync_${SYNC_ARCH}.tar.gz" \
 		&& su-exec $UID:$GID tar xf /app/sync.tar.gz -C /app \
 		&& su-exec $UID:$GID rm -f /app/sync.tar.gz
 }
 
-function rsrepl() {
+rsrepl()
+{
 	su-exec $UID:$GID \
 	curl -s -o /app/version_new.txt -L "https://download-cdn.getsync.com/${SYNC_VER}/linux-${SYNC_ARCH}/version.txt"
 	if [[ -f /app/version.txt && -f /app/version_new.txt ]]; then
 		mapfile -t < /app/version.txt
-		OLDV=${MAPFILE[0]}
+		local OLDV=${MAPFILE[0]}
 		mapfile -t < /app/version_new.txt
-		NEWV=${MAPFILE[0]}
-		echo "Old Version: ${OLDV}"
-		echo "New Version: ${NEWV}"
+		local NEWV=${MAPFILE[0]}
+		log "old version: ${OLDV}"
+		log "new version: ${NEWV}"
 		if [[ "${OLDV}" == "${NEWV}" ]]; then
 			rm -f /app/version_new.txt;
-			echo "Version is current, skipping download."
+			log "version is current, skipping download."
 		else
-			echo "Removing ${OLDV} and installing ${NEWV}."
+			log "removing ${OLDV} and installing ${NEWV}."
 			rm -f /app/*
 			rsinst
 		fi
 	fi
-
 }
+
+fixperms()
+{
+	if [[ "${CHANGE_SYNC_DIR_OWNERSHIP,,}" = "true" ]]; then
+		log "forcing permission to ${UID}:${GID} on /sync folder"
+		chown -R $UID:$GID /sync
+	fi
+	log "fixing UID/GID and permissions of /config and /app"
+	usermod -u $UID $UNAME &> /dev/null \
+		&& groupmod -g $GID $UNAME &> /dev/null \
+		&& chown -R $UID:$GID /config /app
+}
+
+log "setting timezone to ${TZ}"
+echo ${TZ} > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+fixperms
 
 if [[ ! -e /app/rslsync ]]; then
 	rsinst
@@ -38,4 +63,4 @@ else
 	rsrepl
 fi
 
-su-exec $UID:$GID "$@"
+exec su-exec $UID:$GID "$@"
