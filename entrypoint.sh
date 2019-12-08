25f8747b91b74b4fd11ac417670d347152e7707d
Thorsten Schubert <tschubert@bafh.org>
2019-12-08 21:20:11 +0100

    Minor fixes for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,12 @@ log()
 
 spriv()
 {
-	command setpriv --reuid $UID --regid $GID --clear-groups --reset-env "$@"
+	command setpriv --reuid "${UID}" --regid "${GID}" --clear-groups --reset-env "$@"
 }
 
 rsinst()
 {
-	log "downloading resilio-sync ${SYNC_VER} for ${SYNC_ARCH}."
+	log "Downloading resilio-sync ${SYNC_VER} for ${SYNC_ARCH}."
 	spriv curl -s -o /app/version.txt -L "https://download-cdn.getsync.com/stable/linux-x64/version.txt" \
 		&& curl -s -o /app/sync.tar.gz -L "https://download-cdn.getsync.com/${SYNC_VER}/linux-${SYNC_ARCH}/resilio-sync_${SYNC_ARCH}.tar.gz" \
 		&& spriv tar xf /app/sync.tar.gz -C /app \
@@ -29,13 +29,13 @@ rsrepl()
 		local OLDV=${MAPFILE[0]}
 		mapfile -t < /app/version_new.txt
 		local NEWV=${MAPFILE[0]}
-		log "old version: ${OLDV}"
-		log "new version: ${NEWV}"
+		log "Old version: ${OLDV}"
+		log "New version: ${NEWV}"
 		if [[ "${OLDV}" == "${NEWV}" ]]; then
 			rm -f /app/version_new.txt;
-			log "version is current, skipping download."
+			log "Version is current, skipping download."
 		else
-			log "removing ${OLDV} and installing ${NEWV}."
+			log "Removing ${OLDV} and installing ${NEWV}."
 			rm -f /app/*
 			rsinst
 		fi
@@ -45,25 +45,29 @@ rsrepl()
 fixperms()
 {
 	if [[ "${CHANGE_SYNC_DIR_OWNERSHIP,,}" = "true" ]]; then
-		log "forcing permission to ${UID}:${GID} on /sync folder"
-		chown -R $UID:$GID /sync
+		log "Forcing permission to ${UID}:${GID} on /sync folder"
+		chown -R "${UID}":"${GID}" /sync
 	fi
-	log "fixing UID/GID and permissions of /config and /app"
-	usermod -u $UID $UNAME &> /dev/null \
-		&& groupmod -g $GID $UNAME &> /dev/null \
-		&& chown -R $UID:$GID /config /app
+	log "Fixing UID/GID and permissions of /config and /app"
+	usermod -u "${UID}" "${UNAME}" &> /dev/null \
+		&& groupmod -g "${GID}" "${UNAME}" &> /dev/null \
+		&& chown -R "${UID}":"${GID}" /config /app
 }
 
 settime()
 {
-	log "setting timezone to ${TZ}"
-	ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+	log "Setting timezone to ${TZ}"
+	ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime && echo "${TZ}" > /etc/timezone
 	dpkg-reconfigure -f noninteractive tzdata 2>&1
 }
 
 settime
 fixperms
 
-[[ ! -e /app/rslsync ]] && rsinst || rsrepl
+if [[ ! -e /app/rslsync ]]; then
+	rsinst
+else
+	rsrepl
+fi
 
-exec setpriv --reuid $UID --regid $GID --clear-groups --reset-env "$@"
+exec setpriv --reuid "${UID}" --regid "${GID}" --clear-groups --reset-env "$@"
